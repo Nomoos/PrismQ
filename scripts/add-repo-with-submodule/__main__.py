7d900e334bdbb2e5b6fafb1bab41939ee361a093
Nomoos <hittl.r@gmail.com>
2025-10-17 20:38:59 +0200

    Clean up

--- scripts/add-repo-with-submodule/__main__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Entry point when running as a module with python -m add_repo_with_submodule."""
-
-from cli import cli_main
-
-if __name__ == "__main__":
-    cli_main()
--- scripts/submodule-converter/__main__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Entry point for running subtree-converter as a module."""
-
-from .cli import main
-
-if __name__ == "__main__":
-    main()
