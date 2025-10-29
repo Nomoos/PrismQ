0d27898e8eec823caeabb945eaa62f6c95a46f86
Nomoos <hittl.r@gmail.com>
2025-10-29 12:02:01 +0100

    WIP: local changes in IdeaInspiration (issues removed, new config/meta)

---
+++ IdeaInspiration/ConfigLoad/__init__.py
@@ -0,0 +1,17 @@
+"""ConfigLoad - Centralized configuration loading for PrismQ modules.
+
+This module provides centralized .env file management and loading functionality
+for all PrismQ modules, based on the pattern from YouTubeShortsSource.
+"""
+
+from .config import Config
+from .logging_config import ModuleLogger, get_module_logger, setup_basic_logging
+
+__all__ = [
+    "Config",
+    "ModuleLogger",
+    "get_module_logger",
+    "setup_basic_logging",
+]
+
+__version__ = "0.1.0"
