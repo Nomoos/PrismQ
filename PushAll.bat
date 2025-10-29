d2b9e19df36f62bad68eef960e6ae07f70f76eb7
Nomoos <hittl.r@gmail.com>
2025-10-29 13:18:49 +0100

    Remove all subtrees

--- PushAll.bat
+++
@@ -1,6 +0,0 @@
-# push změn zpět
-git subtree push --prefix=IdeaInspiration/Classification                             https://github.com/Nomoos/PrismQ.IdeaInspiration.Classification.git                             main
-git subtree push --prefix=IdeaInspiration/Model                                      https://github.com/Nomoos/PrismQ.IdeaInspiration.Model.git                                      main
-git subtree push --prefix=IdeaInspiration/Scoring                                    https://github.com/Nomoos/PrismQ.IdeaInspiration.Scoring.git                                    main
-git subtree push --prefix=IdeaInspiration/Sources                                    https://github.com/Nomoos/PrismQ.IdeaInspiration.Sources.git                                    main
-git subtree push --prefix=IdeaInspiration/Sources/Content/Shorts/YouTubeShortsSource https://github.com/Nomoos/PrismQ.IdeaInspiration.Sources.Content.Shorts.YouTubeShortsSource.git main
