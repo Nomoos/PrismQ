c45d471a96279fce78e903b907c6142b276fa531
Nomoos <hittl.r@gmail.com>
2025-12-01 10:54:13 +0100

    Move to right place

--- T/State/constants/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Constants module for state management.
-
-Contains state name constants and categories for the workflow state machine.
-"""
-
-from T.State.constants.state_names import StateNames, StateCategory
-
-__all__ = ["StateNames", "StateCategory"]
